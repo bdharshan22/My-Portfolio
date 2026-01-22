804a8658b3cd4d4b8ed0cf08c51baddd96dc71e3
bdharshan22 <b.dharshan0205@gmail.com>
2026-01-22 15:39:31 +0530

    Error Fixed

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 
 interface SplashScreenProps {
   onComplete: () => void;
